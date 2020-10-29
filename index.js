f2a8f22e0764d3352c870c7471c6a5df2d6636c9
Chloe Steffens <caforonda103@yahoo.com>
2020-10-29 18:59:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,39 @@ class Polygon {
     return this.sides.reduce((sum, currentSide) => sum + currentSide, 0)
   }
 }
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.countSides === 3) {
+      let a = this.sides[0]
+      let b = this.sides[1]
+      let c = this.sides[2]
+      let calculateSide = !((a + b < c) || (a + c < b) || (b + c < a))
+      return calculateSide
+    } else {
+      return false
+    }
+  }
+}
+
+
+class Square extends Polygon {
+  get area() {
+    for(const eachSide of this.sides) {
+      return eachSide ** 2;
+    }
+  }
+
+  get isValid() {
+    if (this.countSides === 4) {
+      let a = this.sides[0]
+      let b = this.sides[1]
+      let c = this.sides[2]
+      let d = this.sides[3]
+      let checkSide = (a === b) && (a === c) && (a === d)
+      return checkSide
+    } else {
+      return false
+    }
+  }
+}
